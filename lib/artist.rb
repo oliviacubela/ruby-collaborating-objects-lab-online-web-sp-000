3741edbdd04d20ba48fe94c18bdbb549af1112f4
Olivia Cubela <oliviacubela@gmail.com>
2020-07-13 16:35:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,6 +19,6 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.detect.all {|artist| artist.name == name} || Artist.new(name)
+    self.all.detect {|artist| artist.name == name} || Artist.new(name)
   end
 end
